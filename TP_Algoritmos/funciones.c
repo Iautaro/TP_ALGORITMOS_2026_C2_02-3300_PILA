dc0180ba7715f622a85f03efa0b6c9e882d5fc5c
Iautaro <ladelia@alumno.unlam.edu.ar>
2026-09-21 23:15:08 -0300

    Validacion de comando ingresado por operario

---
+++ TP_Algoritmos/funciones.c
@@ -0,0 +1,40 @@
+#include "funciones.h"
+#include <ctype.h>
+#include <string.h>
+
+#define CANT_COMANDOS 5
+
+void mostrar_operaciones()
+{
+    printf("\nOperaciones y comandos disponibles:\n");
+    printf("\nDES - Descargar y almacenar.");
+    printf("\nREU - Reubicar.");
+    printf("\nENT - Entregar.");
+    printf("\nVER - Ver estado.");
+    printf("\nESP - Esperar.");
+    printf("\nPor favor ingrese comando a ejecutar:");
+
+}
+
+int validar_operacion( char comando[])
+{
+    int i=0;
+    //char comandoValidos[6][4] = {"DES","REU","ENT","VER", "ESP"};
+    const char *comandosValidos[CANT_COMANDOS] = {"DES","REU","ENT","VER", "ESP"};
+
+    for( i;comando[i]!='\0';i++)
+    { // Paso a mayuscula
+        comando[i] = toupper(comando[i]);
+    }
+
+    int c=0;
+    for(c;c<CANT_COMANDOS;c++)
+    {
+        if(strcmp(comando,comandosValidos[c]) == 0 )
+        {
+            return 1; // Comando existe
+        }
+    }
+
+    return 0; // Comando erroneo
+}
---
+++ TP_Algoritmos/funciones.h
@@ -0,0 +1,6 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+void mostrar_operaciones();
+int validar_operacion( char comando[]);
+
+#endif // FUNCIONES_H_INCLUDED
